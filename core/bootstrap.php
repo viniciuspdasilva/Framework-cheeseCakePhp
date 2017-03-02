5b5061106ee9b40d44000ed5b06e20b66db746df
Vinicius Pereira da Silva <viniciussilva32171@gmail.com>
2017-03-02 13:11:57 -0300

    Atualização, inclusão das views, com layout Funcionais

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 
     require_once __DIR__."/../vendor/autoload.php";
+    error_reporting(1);
+    ini_set('display_errors', 1);
 
     $routes = require_once __DIR__."/../app/routes.php";
     $route = new Core\Routes($routes);
